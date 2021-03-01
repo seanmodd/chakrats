bd29e6e8b70a9cb75692ebedaad187a936e11c0b
Sean <sean@vestaridefair.com>
2021-03-01 13:52:47 -0800

    April 1 1:55PM

--- styles/theme.js
+++ styles/theme.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-dupe-keys */
 /* eslint-disable no-undef */
 import { extendTheme, theme as chakraTheme } from '@chakra-ui/react';
 import { createBreakpoints } from '@chakra-ui/theme-tools';
@@ -10,30 +11,30 @@ const fonts = {
     'Inter,-apple-system,BlinkMacSystemFont,"Segoe UI",Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol"',
 };
 
-// const breakpoints = createBreakpoints([
-//   sm: '40em',
-//   md: '52em',
-//   lg: '64em',
-//   // xl: '62em',
-// ]);
-// const fontWeights = {
-//   normal: 300,
-//   medium: 600,
-//   bold: 800,
-// };
+const breakpoints = createBreakpoints({
+  sm: '40em',
+  md: '52em',
+  lg: '64em',
+  lg: '62em',
+});
+const fontWeights = {
+  normal: 300,
+  medium: 600,
+  bold: 800,
+};
 
-// const fontSizes = {
-//   xs: '12px',
-//   sm: '14px',
-//   md: '16px',
-//   lg: '18px',
-//   xl: '20px',
-//   // '2xl': '24px',
-//   // '3xl': '28px',
-//   // '4xl': '36px',
-//   // '5xl': '48px',
-//   // '6xl': '64px',
-// };
+const fontSizes = {
+  xs: '12px',
+  sm: '14px',
+  md: '16px',
+  lg: '18px',
+  xl: '20px',
+  '2xl': '24px',
+  '3xl': '28px',
+  '4xl': '36px',
+  '5xl': '48px',
+  '6xl': '64px',
+};
 
 // const overrides = {
 //   ...chakraTheme,
@@ -43,6 +44,6 @@ const fonts = {
 //   // breakpoints,
 // };
 
-const customTheme = extendTheme({ fonts });
+const customTheme = extendTheme({ fonts, fontWeights, breakpoints, fontSizes });
 
 export default customTheme;
