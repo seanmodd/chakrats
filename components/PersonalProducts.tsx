31a267c41ef4f6abbcc64311e5a3fd29a942b191
Sean <sean@vestaridefair.com>
2021-03-01 17:09:39 -0800

    March 1 4:07PM

--- components/PersonalProducts.tsx
+++ components/PersonalProducts.tsx
@@ -1,31 +1,30 @@
 /* eslint-disable react/jsx-filename-extension */
 /* eslint-disable react/prop-types */
 import { Text } from '@chakra-ui/react';
-import Head from 'next/head'
-import Link from 'next/link'
+import Head from 'next/head';
+import Link from 'next/link';
 import products from '../products.json';
-import styles from '../styles/Home.module.css'
-import { fromImageToUrl } from '../utils/urls'
-import { twoDecimals } from '../utils/format'
+import styles from '../styles/Home.module.css';
+import { fromImageToUrl, API_URL } from '../utils/urls';
+import { twoDecimals } from '../utils/format';
 
 const Catalog = ({ products }) => (
   <ul>
-      {products.map(product => (
-        <div className={styles.product}>
-          <Link href={`/products/${product.slug}`}>
-            <a>
-              <div className={styles.product__Rows}>
-                <div className={styles.product__ColImg}>
-                  <img src={fromImageToUrl(product.image)} />
-                </div>
-                <div className={styles.product__Col}>
-                  {product.name} ${twoDecimals(product.price)}
-                </div>
+    {products.map((product) => (
+      <div className={styles.product}>
+        <Link href={`/products/${product.slug}`}>
+          <a>
+            <div className={styles.product__Rows}>
+              <div className={styles.product__ColImg}>
+                <img src={fromImageToUrl(product.image)} />
               </div>
-            </a>
-          </Link>
-        </div>
-      ))}
+              <div className={styles.product__Col}>
+                {product.name} ${twoDecimals(product.price)}
+              </div>
+            </div>
+          </a>
+        </Link>
+      </div>
     ))}
   </ul>
 );
